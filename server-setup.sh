c6cf254e796002157e1295b9f8cea5e3240afeb3
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-01-07 19:12:06 +0000

    Add conditions for non-apt installs and app dependency caching

--- server-setup.sh
+++ server-setup.sh
@@ -9,6 +9,7 @@ function echo_section_header() {
 
 
 function setup_ufu() {
+     echo_section_header "Configuring UFW"
     sudo ufw allow 'Nginx Full'
     sudo ufw allow 'OpenSSH'
     sudo ufw delete allow 'Nginx HTTP'
@@ -20,25 +21,81 @@ function configure_git() {
           github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
           github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=" >> ~/.ssh/known_hosts
 }
-function get_env_data() {
+function get_data() {
     echo 'First we need to get some information about your environment'
     read -p 'What is your domain name? (e.g. example.com): ' domain
     read -p 'Please enter a FontAwesome Pro token: ' fontAwesomeToken
+    export domain
+    export fontAwesomeToken
 
+}
+function install_packages() {
+   #Disable interactive prompts
+   sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+
+    echo_section_header "Updating system"
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt update
+
+    echo_section_header "Installing PHP"
+    sudo apt install php8.3 php8.3-cli php8.3-{bz2,curl,fpm,mbstring,mysql,redis,intl,xml,zip} -y
+
+    echo_section_header "Installing Composer"
+    if ! [ -f /usr/local/bin/composer ]; then
+        curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+    fi
+
+    echo_section_header "Installing NodeJS"
+    if ! [ -f /usr/bin/node ]; then
+        sudo apt install nodejs npm -y
+        npm install -g n
+        sudo n stable
+    fi
+
+    echo_section_header "Installing Supervisor"
+    sudo apt install supervisor -y
+
+    echo_section_header "Installing Nginx"
+    sudo apt install nginx -y
+
+    echo_section_header "Installing Certbot"
+    sudo apt install certbot python3-certbot-nginx -y
+
+    echo_section_header "Installing MySQL"
+    sudo apt install mysql-server -y
+
+    echo_section_header "Installing Redis"
+    sudo apt install redis-server -y
 }
 function clone_repo() {
   echo_section_header "Cloning SupportManager"
+  if [ -d /var/www/"$domain" ]; then
+      sudo rm -r /var/www/"$domain"
+  fi
   cd /var/www || exit
   git clone git@github.com:TomEasterbrook/support-manager-web.git "$domain"
-  cd support-manager-web || exit
-  git checkout develop
-  composer install --no-scripts
-  npm config set "@fortawesome:registry" https://npm.fontawesome.com/
-  npm config set '//npm.fontawesome.com/:_authToken' "$fontAwesomeToken"
-  npm install
-  npm run build
-  composer dump-autoload -o
-  cd ~ || exit
+}
+function install_app() {
+      cd /var/www/"$domain"|| exit
+      git checkout develop
+      if [ -d ~/cache ]; then
+          cp -r ~/cache/vendor /var/www/"$domain"/vendor
+          cp -r ~/cache/node_modules /var/www/"$domain"/node_modules
+          else
+            echo "No cache of vendor and node_modules found - installing from scratch"
+            composer install --no-scripts
+            npm config set "@fortawesome:registry" https://npm.fontawesome.com/
+            npm config set '//npm.fontawesome.com/:_authToken' "$fontAwesomeToken"
+            npm install
+            mkdir ~/cache
+            cp -r /var/www/"$domain"/vendor ~/cache/vendor
+            cp -r /var/www/"$domain"/node_modules ~/cache/node_modules
+            echo "Cache of dependencies created"
+      fi
+
+      npm run build
+      composer dump-autoload -o
+      cd ~ || exit
 }
 
 echo_section_header "Welcome to the SupportManager server setup script"
@@ -50,41 +107,9 @@ This script will install all the required dependencies for the SupportManager to
 read -p "Press enter to continue"
 
 get_env_data
-
+install_packages
 configure_git
 
-sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-
-echo_section_header "Updating system"
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt update
-
-echo_section_header "Installing PHP"
-sudo apt install php8.3 php8.3-cli php8.3-{bz2,curl,fpm,mbstring,mysql,redis,intl,xml,zip} -y
-
-echo_section_header "Installing Composer"
-sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
-echo_section_header "Installing NodeJS"
-sudo apt install nodejs npm -y
-npm install -g n
-sudo n stable
-
-echo_section_header "Installing Supervisor"
-sudo apt install supervisor -y
-
-echo_section_header "Installing Nginx"
-sudo apt install nginx -y
-
-echo_section_header "Installing Certbot"
-sudo apt install certbot python3-certbot-nginx -y
-
-echo_section_header "Installing MySQL"
-sudo apt install mysql-server -y
-
-echo_section_header "Installing Redis"
-sudo apt install redis-server -y
-
-echo_section_header "Configuring UFW"
 setup_ufu
 clone_repo
+install_app
