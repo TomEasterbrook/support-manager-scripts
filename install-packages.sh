084eda60db6ed1424345074ed8fbdeabbd110b5b
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-01-14 14:25:10 +0000

    Move package installation to separate script

---
+++ install-packages.sh
@@ -0,0 +1,86 @@
+function update_repo() {
+    echo_section_header "Updating repository"
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt update
+
+}
+
+function install_php() {
+     if ! [ -f /etc/php/8.3/fpm/php.ini ]; then
+            echo_section_header "Installing PHP"
+            sudo apt install php7.4 php7.4-fpm php7.4-mysql php7.4-redis php7.4-mbstring php7.4-xml php7.4-curl php7.4-zip php7.4-gd php7.4-bcmath php7.4-intl php7.4-imagick -y
+            else
+            echo_section_header "PHP already installed"
+        fi
+        if ! [ -f /usr/local/bin/composer ]; then
+            echo_section_header "Installing Composer"
+            curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+            else
+            echo_section_header "Composer already installed"
+        fi
+
+}
+
+function install_npm() {
+        if ! [ -f /usr/bin/node ]; then
+            echo_section_header "Installing NodeJS"
+            sudo apt install nodejs npm -y
+            npm install -g n
+            sudo n stable
+            else
+            echo_section_header "NodeJS already installed"
+        fi
+}
+function install_supervisor() {
+    if ! [ `command -v "supervisorctl"` ]; then
+        echo_section_header "Installing Supervisor"
+        sudo apt install supervisor -y
+        else
+        echo_section_header "Supervisor already installed"
+    fi
+}
+function install_webserver() {
+     if ! [ `command -v "nginx"` ]; then
+          echo_section_header "Installing Nginx"
+          sudo apt install nginx -y
+          else
+          echo_section_header "Nginx already installed"
+
+      fi
+
+      if ! [ `command -v "certbot"` ]; then
+          echo_section_header "Installing Certbot"
+          sudo apt install certbot python3-certbot-nginx -y
+          else
+          echo_section_header "Certbot already installed"
+      fi
+
+}
+
+function install_databases() {
+      if ! [ `command -v "mysql"` ]; then
+          echo_section_header "Installing MySQL"
+          sudo apt install mysql-server -y
+          else
+          echo_section_header "MySQL already installed"
+      fi
+
+      if ! [ `command -v "redis-server"` ]; then
+          echo_section_header "Installing Redis"
+          sudo apt install redis-server -y
+          else
+          echo_section_header "Redis already installed"
+      fi
+
+}
+
+function install_packages() {
+   #Disable interactive prompts
+   sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+    update_repo
+    install_php
+    install_npm
+    install_supervisor
+    install_webserver
+    install_databases
+}
--- setup.sh
+++ setup.sh
@@ -28,74 +28,6 @@ function get_data() {
     export fontAwesomeToken
 
 }
-function install_packages() {
-   #Disable interactive prompts
-   sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-
-    echo_section_header "Updating system"
-    sudo add-apt-repository ppa:ondrej/php -y
-    sudo apt update
-
-
-    if ! [ -f /etc/php/8.3/fpm/php.ini ]; then
-        echo_section_header "Installing PHP"
-        sudo apt install php7.4 php7.4-fpm php7.4-mysql php7.4-redis php7.4-mbstring php7.4-xml php7.4-curl php7.4-zip php7.4-gd php7.4-bcmath php7.4-intl php7.4-imagick -y
-        else
-        echo_section_header "PHP already installed"
-    fi
-
-    if ! [ -f /usr/local/bin/composer ]; then
-        echo_section_header "Installing Composer"
-        curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-        else
-        echo_section_header "Composer already installed"
-    fi
-
-    if ! [ -f /usr/bin/node ]; then
-        echo_section_header "Installing NodeJS"
-        sudo apt install nodejs npm -y
-        npm install -g n
-        sudo n stable
-        else
-        echo_section_header "NodeJS already installed"
-    fi
-
-    if [ `command -v "supervisorctl"` ]; then
-        echo_section_header "Supervisor already installed"
-        else
-        echo_section_header "Installing Supervisor"
-        sudo apt install supervisor -y
-    fi
-
-    if [ `command -v "nginx"` ]; then
-        echo_section_header "Nginx already installed"
-        else
-        echo_section_header "Installing Nginx"
-        sudo apt install nginx -y
-
-    fi
-
-    if [ `command -v "certbot"` ]; then
-        echo_section_header "Certbot already installed"
-        else
-        echo_section_header "Installing Certbot"
-        sudo apt install certbot python3-certbot-nginx -y
-    fi
-
-    if [ `command -v "mysql"` ]; then
-        echo_section_header "MySQL already installed"
-        else
-        echo_section_header "Installing MySQL"
-        sudo apt install mysql-server -y
-    fi
-
-    if [ `command -v "redis-server"` ]; then
-        echo_section_header "Redis already installed"
-        else
-        echo_section_header "Installing Redis"
-        sudo apt install redis-server -y
-    fi
-}
 function clone_repo() {
   echo_section_header "Cloning SupportManager"
   if [ -d /var/www/"$domain" ]; then
@@ -123,7 +55,10 @@ function install_app() {
         composer dump-autoload -o
         cp .env.example .env
         php artisan key:generate
-
+        chmod -R 777 storage bootstrap/cache
+        cd storage/ || exit
+        mkdir -p framework/{sessions,views,cache}
+        chmod -R 775 framework
         if ! [ -d ~/cache ]; then
                     mkdir -p ~/cache
                     cp -r /var/www/"$domain"/vendor ~/cache/vendor
@@ -139,19 +74,27 @@ function configure_nginx() {
     cp ~/support-manager-scripts/support/nginx.conf /etc/nginx/sites-available/"$domain".conf
     sed -i "s/{{DOMAIN}}/$domain/g" /etc/nginx/sites-available/"$domain".conf
     ln -s /etc/nginx/sites-available/"$domain".conf /etc/nginx/sites-enabled/
-    unlink /etc/nginx/sites-enabled/default
+    echo_section_header "Configuring SSL"
+    echo "Please follow the prompts to configure SSL. Press enter to continue"
+    sudo certbot --nginx -d "$domain"
     sudo systemctl restart nginx
 }
+function configure_supervisor() {
+    echo_section_header "Configuring Supervisor"
+    sudo cp ~/support-manager-scripts/support/supervisor.conf /etc/supervisor/conf.d/"$domain".conf
+    sudo sed -i "s/{{DOMAIN}}/$domain/g" /etc/supervisor/conf.d/"$domain".conf
+    sudo supervisorctl reread
+    sudo supervisorctl update
+    sudo supervisorctl start "$domain"
+}
 
-cd ~ || exit
 echo_section_header "Welcome to the SupportManager server setup script"
 echo "Welcome to the SupportManager the server setup script
 This script will install all the required dependencies for the SupportManager to run"
 
 read -p "Press enter to continue"
-
 get_data
-install_packages
+sudo bash install-packages.sh
 configure_git
 
 setup_ufu
