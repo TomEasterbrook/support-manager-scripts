ad14e734dad7c99e862febbb4136c9aa05c93e8e
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-01-14 20:04:35 +0000

    Update mysql-setup.sql

--- support/mysql-setup.sql
+++ support/mysql-setup.sql
@@ -1,8 +1,8 @@
-UPDATE mysql.user SET Password=PASSWORD('{{DB_ROOT_PASS}}') WHERE User='root';
+UPDATE mysql.user SET Password=PASSWORD('{{DB_ROOT_PASSWORD}}') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 CREATE USER '{{DB_USERNAME}}'@'localhost' IDENTIFIED BY '{{DB_PASSWORD}}';
 CREATE DATABASE IF NOT EXISTS '{{DB_DATABASE}}';
-GRANT ALL PRIVILEGES ON{{DB_DATABASE}}.* TO 'SupportManager'@'localhost';
+GRANT ALL PRIVILEGES ON {{DB_DATABASE}}.* TO 'SupportManager'@'localhost';
 FLUSH PRIVILEGES;
