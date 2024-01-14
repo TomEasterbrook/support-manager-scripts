bc4355a37c50a2c6a7d20a9f5fe0387e9532a649
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-01-14 16:02:36 +0000

    Fix SQL template

--- support/mysql-setup.sql
+++ support/mysql-setup.sql
@@ -1,4 +1,4 @@
-CREATE USER 'SupportManager'@'localhost' IDENTIFIED BY '{{$Password}}';
+CREATE USER 'SupportManager'@'localhost' IDENTIFIED BY '{{DB_PASSWORD}}';
 CREATE DATABASE IF NOT EXISTS SupportManager;
 GRANT ALL PRIVILEGES ON SupportManager.* TO 'SupportManager'@'localhost';
 FLUSH PRIVILEGES;
